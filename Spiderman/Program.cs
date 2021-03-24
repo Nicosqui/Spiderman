b5231267ee5790ff2e1d184290ab189a12da8275
Nicosqui <nico50829@gmail.com>
2021-03-23 23:39:55 -0500

    first commit

---
+++ Spiderman/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Spiderman
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of buildings: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Spiderman has to go through " + number + " buildings and has " + CalculatePossiblePaths(number, number) 
+                    + " possible paths");
+
+        }
+
+        static int CalculatePossiblePaths(int b, int number)
+        {
+            //Console.WriteLine("Current Building = " + b);
+            Console.WriteLine(number - b);
+
+            if (b >= 2)
+            {
+                //Console.WriteLine(b-(b-1));
+                //Console.WriteLine(b -(b- 2));
+                return CalculatePossiblePaths(b - 1, number) + CalculatePossiblePaths(b - 2, number);
+            }
+            else if (b == 1)
+            {
+                //Console.WriteLine(b-(b-1));
+                return CalculatePossiblePaths(b - 1, number);
+            }
+            // There are no buildings left, a path has been completed.
+            else
+            {
+                return 1;
+            }
+        }
+
+    }
+}
